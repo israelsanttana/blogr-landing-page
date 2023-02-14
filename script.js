150f6aed7fb46f346648d92de57a3b25b91adedb
israelsanttana <israelsantanasantos7@gmail.com>
2023-02-13 23:34:32 -0300

    add menu mobile

---
+++ script.js
@@ -0,0 +1,7 @@
+const nav = document.getElementById('nav');
+const user = document.getElementById('user');
+const btnMenu = document.getElementById('btn-menu')
+
+function activeMenu() {
+    nav.classList.add('hidden');
+}
