b4f4a0b920bb9a39e3930ab4294eba8f1e564196
israelsanttana <israelsantanasantos7@gmail.com>
2023-02-14 20:56:25 -0300

    menu e icon mobile implementados/ css menu mobile finalizado

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
+const open = document.getElementById("open");
+const close = document.getElementById('close');
 
+open.addEventListener('click', openMenu);
+close.addEventListener('click', openMenu);
 
-function activeMenu() {
+function openMenu() {
     const nav = document.getElementById('nav');
-    
-    if(nav.style.display === "none") {
-        nav.style.display = "block"
-        
-    } else {
-        nav.style.display = "none"
-    }
-   
+    nav.classList.toggle('active');
+    open.classList.toggle('hidden');
+    close.classList.toggle('active');
+
 }
 
